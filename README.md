7d9b9f29b4a7a06ce65f5d999e6c1a886991bbf7
loconox <contact@jeremielibeau.fr>
2017-07-26 17:03:11 +0200

    Amélioration du script et de la doc

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 Rechargement TER standalone
 ===========================
 
-Ce script permet de récupérer l'application Java et de la lancer sans avoir besoin d'être sous windows et de passer par l'installateur.
+Ce script permet de récupérer l'application Java et de la lancer sans avoir besoin d'être sous Windows et de passer par l'installateur.
+
+# Compatibilité
+
+Cette application est compatible avec les solutions suivantes :
+* Korrigo - Région Bretagne : https://www.ter.sncf.com/bretagne
+
 
 # Dépendances
 
 * curl
 * java
-* innoextract
+* innoextract (voir http://constexpr.org/innoextract/)
+
 
 # Utilisation
 
--- run.bash
+++ run.bash
@@ -1,8 +1,58 @@
 #!/bin/bash
 
-echo "Récupération de l'application .exe"
-curl -O https://www.ter.ritmx.sncf.com/cache/installer/setup-rechargement-SNCF-TER-v1_9-x32.exe
-echo "Extraction des assets"
-/usr/local/Cellar/innoextract/1.6/bin/innoextract setup-rechargement-SNCF-TER-v1_9-x32.exe
-echo "Lancement de l'application"
-java -jar app/app/clientDomter-jfx.jar
+EXE_FILENAME="setup-rechargement-SNCF-TER-v1_9-x32.exe"
+EXE_URL="https://www.ter.ritmx.sncf.com/cache/installer/$EXE_FILENAME"
+JAR_FILENAME="app/app/clientDomter-jfx.jar"
+
+function dependenciesCheck() {
+	# curl
+	if command -v curl >/dev/null 2>&1 ; then
+	    printf "curl \e[32mfound\e[0m\n"
+	else
+	    printf "curl \e[31mnot found\e[0m\n"
+	    echo "Please install curl yourself"
+		exit 1
+	fi
+	
+	# java
+	if command -v java >/dev/null 2>&1 ; then
+	    printf "java \e[32mfound\e[0m\n"
+	else
+	    printf "java \e[31mnot found\e[0m\n"
+		echo "Please install java yourself"
+		exit 1
+	fi
+	
+	# innoextract
+	if command -v innoextract >/dev/null 2>&1 ; then
+	    printf "innoextract \e[32mfound\e[0m\n"
+	else
+	    printf "innoextract \e[31mnot found\e[0m\n"
+		echo "Please install innoextract yourself"
+		exit 1
+	fi
+}
+
+echo "Dependencies check ..."
+dependenciesCheck
+
+echo -e "\nDownloading .exe app from website ..."
+curl -O $EXE_URL
+if [ $? -gt 0 ]; then
+	printf "Download \e[31mfailed\e[0m\n"
+	exit 1
+fi
+
+echo -e "\nExtracting .exe assets ..."
+innoextract $EXE_FILENAME
+if [ $? -gt 0 ]; then
+	printf "Application extraction \e[31mfailed\e[0m\n"
+	exit 1
+fi
+
+echo -e "\nRun the application ..."
+java -jar $JAR_FILENAME
+if [ $? -gt 0 ]; then
+	printf "Application launch \e[31mfailed\e[0m\n"
+	exit 1
+fi
